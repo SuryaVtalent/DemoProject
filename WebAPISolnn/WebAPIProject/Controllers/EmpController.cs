fd961c4a8fc6ff693bb8e81b0623190ef37201f2
SuryaVtalent <suryaa.vtalent@gmail.com>
2024-07-25 16:21:30 +0530

    A new Controller created "EmpController"

---
+++ WebAPISolnn/WebAPIProject/Controllers/EmpController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace WebAPIProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmpController : ControllerBase
+    {
+
+        public IActionResult GetNames()
+        {
+            try
+            {
+                List<string> Names = new List<string>();
+
+                Names.Add("Kohli");
+                Names.Add("Surya");
+                Names.Add("Greeshma");
+                Names.Add("Ivaan");
+                Names.Add("Yuvraj");
+
+                return Ok(Names);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+    }
+}
